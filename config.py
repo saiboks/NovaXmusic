365296507e8f552b7758a80c37bcbdc279577998
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-08-15 13:13:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Roninbyrne/Arthey_Music",
+    "https://github.com/LucyXDbot/NovaXmusic",
 )
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
