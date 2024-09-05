47a2b2fdfbc6ff54e20e5d27a05b6fc9dda28aca
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-05 13:45:39 +0530

    Update repo.py

--- AnonXMusic/plugins/bot/repo.py
+++ AnonXMusic/plugins/bot/repo.py
@@ -4,7 +4,7 @@ from AnonXMusic import app
 from config import BOT_USERNAME
 
 start_txt = """
-𖣐 ʜᴇʏ , ᴛʜᴇʀᴇ ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ  ♥︎\n\n● ɪғ ʏᴏᴜ ᴡᴀɴᴛ ʟ ᴜ ᴄ ʏ • / ‹𝟹, ʙᴏᴛ ʀᴇᴘᴏ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴄᴏʟʟᴇᴄᴛ ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ.\n\n𖣐 ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝐀𝐫𝐭𝐡𝐞𝐲 ♡゙ ᴍ ᴜ s ɪ ᴄ"""
+𖣐 ʜᴇʏ , ᴛʜᴇʀᴇ ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ  ♥︎\n\n● ɪғ ʏᴏᴜ ᴡᴀɴᴛ ʟ ᴜ ᴄ ʏ • / ‹𝟹, ʙᴏᴛ ʀᴇᴘᴏ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴄᴏʟʟᴇᴄᴛ ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ.\n\n𖣐 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ʟ ᴜ ᴄ ʏ • / ‹𝟹"""
 
 
 
@@ -21,7 +21,7 @@ async def start(_, msg):
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://graph.org/file/ec5d7ec6899af85241cd9.jpg",
+        photo="https://telegra.ph/file/1db368f1626456706ae86.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
